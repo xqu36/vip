a2ab7946b69556e98c2860422a5ada88bdd8cd43
maye-vaca <madeleyne.vaca@gmail.com>
2016-03-14 17:47:33 -0400

    updated data logging

--- sensorTesting/IR_US_ADC_test.py
+++ sensorTesting/IR_US_ADC_test.py
@@ -15,12 +15,17 @@ IRvalues = []
 USvalues = []
 
 start = time.time()
-while True: #(time.time() - start) <= 5.0:
+f = open('IR_US_log', 'w')
+while (time.time() - start) <= 10.0:
     # Read the last ADC conversion value and print it out.
-    IRvalues.append(adc.read_adc(0, gain=GAIN))
-    USvalues.append(adc.read_adc(1, gain=GAIN))
+    valIR = adc.read_adc(0, gain=GAIN)
+    valUS = adc.read_adc(1, gain=GAIN)
+    IRvalues.append(valIR)
+    USvalues.append(valUS)
+    f.write('{0}: {1},{2}\n'.format(time.time(),valIR,valUS))
     # Sleep for half a second.
     time.sleep(0.5)
 
 # Stop continuous conversion.  After this point you can't get data from get_last_result!
 adc.stop_adc()
+
