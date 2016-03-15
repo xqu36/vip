cb29f47f52b8df99bac05c0b5a6facd72426c85e
RickyAcosta <rickymacosta@gatech.edu>
2016-03-15 19:12:36 -0400

    reads in data from arduino and formats it in proper .csv

--- sensorTesting/ArduinoRead2csv_v2.py
+++ sensorTesting/ArduinoRead2csv_v2.py
@@ -7,12 +7,11 @@ timeStamp = time.strftime("%d-%m-%Y_%H:%M:%S", time.localtime())
 filename = 'ultrasonic_' + timeStamp + '.csv'
 logfile=open(filename,'a')
 
-with open(filename + '.csv', 'w') as f:
+with logfile as f:
 	w = csv.writer(f)
 	w.writerow(["timestamp","valid","sensor","data1"])
 	arduinoString = arduinoData.readline()
 	while True:
-		sleep(1)
 		if arduinoString is not None:
 			row = [time.time(),1,"ultrasonic",arduinoString]
 			w.writerow(row)
