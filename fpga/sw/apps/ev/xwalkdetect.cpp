56a3d605320f7863d15a853849c70e15c82da896
Ari Bleemer <ableemer@gatech.edu>
2016-03-17 17:28:23 -0400

    implemented watershed

--- fpga/sw/apps/ev/xwalkdetect.cpp
+++ fpga/sw/apps/ev/xwalkdetect.cpp
@@ -31,6 +31,7 @@ int main() {
   Mat out_frame_r, out_frame_g, out_frame_b, frame_road, frame_road_bw;
   Mat road_bw_otsu, road_bw_adapt_gauss, road_bw_adapt, centers;
   capture >> frame;
+  Mat frame_og = frame.clone();
   //frame = imread(infile, 1);
 
   if (!frame.data) { cout << "No image data." << endl; return -1; }
@@ -40,12 +41,12 @@ int main() {
   //Mat rgbSplit2[3];
   split(frame, rgbSplit);
   //rgbSplit2 = rgbSplit;
-  threshold(rgbSplit[0], frame_r, 50, 255, THRESH_TOZERO);
-  threshold(frame_r, frame_r, 120, 255, THRESH_TOZERO_INV);
-  threshold(rgbSplit[1], frame_g, 60, 255, THRESH_TOZERO);
-  threshold(frame_g, frame_g, 110, 255, THRESH_TOZERO_INV);
-  threshold(rgbSplit[2], frame_b, 50, 255, THRESH_TOZERO);
-  threshold(frame_b, frame_b, 110, 255, THRESH_TOZERO_INV);
+  threshold(rgbSplit[0], frame_r, 45, 255, THRESH_TOZERO);
+  threshold(frame_r, frame_r, 125, 255, THRESH_TOZERO_INV);
+  threshold(rgbSplit[1], frame_g, 55, 255, THRESH_TOZERO);
+  threshold(frame_g, frame_g, 115, 255, THRESH_TOZERO_INV);
+  threshold(rgbSplit[2], frame_b, 45, 255, THRESH_TOZERO);
+  threshold(frame_b, frame_b, 115, 255, THRESH_TOZERO_INV);
 
   int r = frame_r.rows;
   int c = frame_r.cols;
@@ -73,9 +74,12 @@ int main() {
 
   rgbSplit[0]=frame_r;
   rgbSplit[1]=frame_g;
+  cout << "before b frame" << endl;
   rgbSplit[2]=frame_b;
-
+  cout << "after b frame" << endl;
   merge(rgbSplit, 3, frame);
+
+  imshow("frame post-thresh", frame);
 
   /* PROCESSING */
     // Create black and white frame
@@ -95,13 +99,69 @@ int main() {
           //dilate(frame, frame, Mat(), Point(-1,-1), 2,1,1);
           erode(frame, frame, Mat(), Point(-1,-1), 2,1,1);
     }
-    for(int i = 0; i < 2; i++){
+    for(int i = 0; i < 3; i++){
           dilate(frame, frame, Mat(), Point(-1,-1), 2,1,1);
           //erode(frame, frame, Mat(), Point(-1,-1), 2,1,1);
     }
   }
-  imshow("morpho", frame);
-  imwrite("/home/ableemer/vip/fpga/sw/apps/ev/klaus_high_roadDetect.JPG", frame);
+  cvtColor(frame, frame, CV_BGR2GRAY);
+  frame.convertTo(frame, CV_8U);
+  imshow("frame", frame);
+
+  Mat dist;
+  distanceTransform(frame, dist, CV_DIST_L2, 3);
+  normalize(dist, dist, 0, 1., NORM_MINMAX);
+  //dist.convertTo(dist, CV_8U);
+  //imshow("Distance Transform", dist);
+  threshold(dist, dist, .25, 1., CV_THRESH_BINARY);
+  Mat kernel = Mat::ones(3, 3, CV_8UC1);
+  dilate(dist, dist, kernel);
+  imshow("peaks", dist);
+  //imwrite("/home/ableemer/vip/fpga/sw/apps/ev/distTransform.JPG", dist);
+  Mat dist_8u;
+  dist.convertTo(dist_8u, CV_8U);
+  vector<vector<Point>> contours;
+  findContours(dist_8u, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+  Mat markers = Mat::zeros(dist.size(), CV_32SC1);
+  for(size_t i = 0; i< contours.size(); i++)
+    drawContours(markers, contours, static_cast<int>(i), Scalar::all(static_cast<int>(i) + 1), -1);
+  circle(markers, Point(5,5), 3, CV_RGB(255, 255, 255), -1);
+  watershed(frame_og, markers);
+  Mat mark = Mat::zeros(markers.size(), CV_8UC1);
+  markers.convertTo(mark, CV_8UC1);
+  bitwise_not(mark, mark);
+  vector<Vec3b> colors;
+  for(size_t i = 0; i < contours.size(); i++)
+  {
+    int b = theRNG().uniform(0,255);
+    int g = theRNG().uniform(0,255);
+    int r = theRNG().uniform(0,255);
+
+    colors.push_back(Vec3b((uchar)b, (uchar)g, (uchar)r));
+  }
+
+  Mat dst = Mat::zeros(markers.size(), CV_8UC3);
+
+  for(int i = 0; i < markers.rows; i++)
+  {
+    for(int j = 0; j < markers.cols; j++)
+    {
+      int index = markers.at<int>(i,j);
+      if(index > 0 && index <= static_cast<int>(contours.size()))
+        dst.at<Vec3b>(i, j) = colors[index - 1];
+      else
+        dst.at<Vec3b>(i, j) = Vec3b(0,0,0);
+    }
+  }
+  imshow("Final Result", dst);
+  imshow("OG frame", frame_og);
+  imwrite("/home/ableemer/vip/fpga/sw/apps/ev/klaus_high_watershed25.JPG", dst);
+  /*dist.convertTo(dist, CV_32S);
+  watershed(frame_og, dist);
+  dist.convertTo(dist, CV_8U);
+  imshow("watershed", dist);
+  */
+  //imwrite("/home/ableemer/vip/fpga/sw/apps/ev/klaus_high_roadDetect.JPG", frame);
 /*
     for(int i = 1; i < 10; i+=2){
       GaussianBlur(frame_bw, blur_frame, Size(i,i), 0, 0);
@@ -140,7 +200,8 @@ int main() {
     //adaptiveThreshold(frame_road_bw, road_bw_adapt_gauss, 255, CV_ADAPTIVE_THRESH_GAUSSIAN_C, CV_THRESH_BINARY, 11, 2);
 
     //imwrite("/home/ableemer/vip/fpga/sw/apps/ev/hemphill_low_roadDetect.JPG", frame_road);
-
+/*
+    KMEANS
     Mat frame2 = frame;
     vector<Mat> channels;
     split(frame2, channels);
@@ -150,15 +211,16 @@ int main() {
     frame2.convertTo(frame2, -1, 1.5, 0);
     imshow("contrast", frame2);
 
-    cvtColor(frame2, frame2, CV_YCrCb2BGR);
+    //cvtColor(frame2, frame2, CV_YCrCb2BGR);
 
     //equalizeHist(dst, dst);
-
+    
     Mat samples(frame2.rows * frame2.cols, 3, CV_32F);
     for( int y = 0; y < frame2.rows; y++ )
       for( int x = 0; x < frame2.cols; x++ )
         for( int z = 0; z < 3; z++)
           samples.at<float>(y + x*frame2.rows, z) = frame2.at<Vec3b>(y,x)[z];
+
 
 
     int clusterCount = 3;
@@ -176,7 +238,7 @@ int main() {
         new_image.at<Vec3b>(y,x)[1] = centers.at<float>(cluster_idx, 1);
         new_image.at<Vec3b>(y,x)[2] = centers.at<float>(cluster_idx, 2);
       }
-
+*/
 
 
     /* Save morpho/threshed image and blurred image
@@ -209,8 +271,8 @@ int main() {
         //cout << "iteration end" << endl;
       }
     //ret.convertTo(ret, CV_8U);*/  
-    imshow("kmeans", new_image);
-    imwrite("/home/ableemer/vip/fpga/sw/apps/ev/klausHigh_splitKmeans.JPG", new_image);
+    //imshow("kmeans", new_image);
+    //imwrite("/home/ableemer/vip/fpga/sw/apps/ev/klausHigh_splitKmeans.JPG", new_image);
 /*    imshow("frame_r", out_frame_r);
     imshow("frame_g", out_frame_g);
     imshow("frame_b", out_frame_b);
