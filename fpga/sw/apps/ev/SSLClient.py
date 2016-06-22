93d36e0470cd9e666d6d1d8eaef9683a9bfeca05
Michael Capone <michaelcapone@csr-loaner-l02.local>
2016-06-22 10:56:13 -0400

    Correction to script to handle RST packets that occur when there is instability in the connection medium (e.g. 4G)

--- fpga/sw/apps/ev/SSLClient.py
+++ fpga/sw/apps/ev/SSLClient.py
@@ -4,38 +4,43 @@ import socket               # Import socket module
 import json
 import ssl
 import pprint
+import time
 
 def send_data(data):
-  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  # Require a certificate from the server. We used a self-signed certificate
-  # so here ca_certs must be the server certificate itself.
-  ssl_sock = ssl.wrap_socket(s,
-	                           ca_certs="node1/cacert.pem",
-	                           cert_reqs=ssl.CERT_REQUIRED, 
-	                           certfile="node1/node1.pem",
-                               keyfile="node1/server.key",
-	                           ciphers= "HIGH",
-	                           do_handshake_on_connect=True)
+  try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Require a certificate from the server. We used a self-signed certificate
+    # so here ca_certs must be the server certificate itself.
+    ssl_sock = ssl.wrap_socket(s,
+                                 ca_certs="node1/cacert.pem",
+                                 cert_reqs=ssl.CERT_REQUIRED, 
+                                 certfile="node1/node1.pem",
+                                 keyfile="node1/server.key",
+                                 ciphers= "HIGH",
+                                 do_handshake_on_connect=True)
 
-  ssl_sock.settimeout(5)
-
-  #ssl_sock.connect(('brandon.gtisc.gatech.edu', 10023))
-  ssl_sock.connect(('smartcities.gatech.edu', 50008))
-  info = data #encrypt_data(data)
-  #print_log(ssl_sock)
-  ssl_sock.write(json.dumps(info))
-
-  ssl_sock.close()
-
-  if False: # from the Python 2.7.3 docs
-    # Set a simple HTTP request -- use httplib in actual code.
-    ssl_sock.write("""GET / HTTP/1.0\r
-    Host: www.verisign.com\n\n""")
-    # Read a chunk of data.  Will not necessarily
-    # read all the data returned by the server.
-    data = ssl_sock.read()
-    # note that closing the SSLSocket will also close the underlying socket
+    ssl_sock.settimeout(1)
+    ssl_sock.connect(('smartcities.gatech.edu', 50008))
+    info = data #encrypt_data(data)
+    #print_log(ssl_sock)
+    ssl_sock.write(json.dumps(info))
+    time.sleep(0.01)
+    #ssl_sock.shutdown(socket.SHUT_RDWR)
     ssl_sock.close()
+
+    if False: # from the Python 2.7.3 docs
+      # Set a simple HTTP request -- use httplib in actual code.
+      ssl_sock.write("""GET / HTTP/1.0\r
+      Host: www.verisign.com\n\n""")
+      # Read a chunk of data.  Will not necessarily
+      # read all the data returned by the server.
+      data = ssl_sock.read()
+      # note that closing the SSLSocket will also close the underlying socket
+      ssl_sock.close()
+  except socket.error, msg:
+    print "Error in send_data!"
+  finally:
+    ssl_sock.close();
 
 def print_log(ssl_sock):
   print repr(ssl_sock.getpeername())
