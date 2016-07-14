324618e88b8e4bee2dc543f4ba29a0d10f606b0d
Chris Turner <cturner48@gatech.edu>
2016-07-14 15:02:52 -0400

    Updates to arduino scripts after V1.0 deployment and code comments.

--- enclosure/arduinoScripts/deadMan/deadMan.ino/deadMan.ino.ino
+++ enclosure/arduinoScripts/deadMan/deadMan.ino/deadMan.ino.ino
@@ -3,61 +3,90 @@
 #include <DS1307RTC.h>
 
 
-// Variable Definitions
+// VARIABLE DEFINITIONS
+//
+// Switch control for the power relay. Connects to S on relay.
 const int relayControl = 8;
+// Outgoing pin for health monitoring. Connects to JA3 on ZedBoard. 
 const int deadManTx = 2;
-//ONLY INPUT PIN = 4
+// Pin 4 is the only input pin to be set in the rc.local
+// in the /etc directory of the OS. Recieves signal from ZedBoard
+// to stop a healthcheck reset.
+// Connects to JA10 on ZedBoard.
 const int deadManRx = 4;
-//const int vOut = 10;
+// Output signal to indicate a forced shutdown of the board is occuring.
+// FPGA will powerdown and wait for power cycle after receiving this signal.
+// Connects to JA9.
 const int forceDown = 6;
+// CountDown used as counter.
+// MissionAbort is value holder for healthcheck canceling.
+// SignalMatch is variable to hold previous status of deadManRX to prevent
+// constant high power resets. 
 int countDown, missionAbort, signalMatch;
+// creates tm structure for time values.
+// tm.Second, tm.Minute, tm.Hour.
 tmElements_t tm;
 
+
+// SETUP LOOP
+//
 void setup() {
   pinMode(8, OUTPUT);
   pinMode(2, OUTPUT);
   pinMode(4, INPUT);
-  //pinMode(10, OUTPUT);
   pinMode(6, OUTPUT);
+  // Initialize serial output for development purposes.
   Serial.begin(9600);
   while (!Serial);
   delay(200);
   Serial.println("-- DISPLAY ONLINE --");
-  //Uncomment for initial setup
+  // setTime function will set the current time of the RTC. Must be used
+  // in a complete circuit with RTC attached. After programming once to
+  // set the time, uncomment to prevent a time reset after each power cycle.
   //setTime();
   
 
 }
 
+// MAIN LOOP
+//
 void loop(){
+  // Initializing all pin values.
   digitalWrite(forceDown, LOW);
-  RTC.read(tm);
-  //digitalWrite(vOut, HIGH);
   digitalWrite(relayControl, HIGH);
   digitalWrite(deadManTx, LOW);
-  //Serial.print(tm.Hour);
-  //Serial.print(tm.Minute);
+  // Grabbing current time from RTC.
+  RTC.read(tm);
+  // Printing time and data for development.
   Serial.print(tm.Hour);
   Serial.write(':');
   Serial.print(tm.Minute);
   Serial.write(':');
   Serial.print(tm.Second);
   Serial.println();
-  if(tm.Second == 0 && tm.Minute == 0){
+  // HEALTH CHECK FUNCTION
+  //
+  // Checks once per hour to make sure the FPGA is responsive.
+  if(tm.Second == 00 && tm.Minute == 00){
+    // Sets signalMatch and missionAbort to the current value of 
+    // deadManRx for comparisson values to deteremine health of board.
     signalMatch = digitalRead(deadManRx);
+    missionAbort = digitalRead(deadManRx);
+    // Health monitor flag activated.
     digitalWrite(deadManTx, HIGH);
+    // Countdown sequence. If the value of deadManRx is not flipped before 900 seconds
+    // the health check sequence will recycle the power of the board.
     for(countDown = 0; countDown < 900; countDown ++) {
-      missionAbort = digitalRead(deadManRx);
       delay(1000);
+      // If statement activated if health of board is ok and no reset needed.
       if(missionAbort != signalMatch) {
         digitalWrite(deadManTx, LOW);
         break;
       } else {
         Serial.write("Houston, we have a problem.");
         Serial.println();
-        
-        
-      } 
+      }
+      // If statement for an unresponsive board, recycles power and waits one second.
       if(countDown == 899 ) {
         digitalWrite(relayControl, LOW);
         digitalWrite(deadManTx, LOW);
@@ -66,16 +95,23 @@ void loop(){
       }
     }
   }
+  // FORCE SHUTDOWN FUNCTION
+  //
+  // Once per day forces a power recycle. 
   if(tm.Second == 00 && tm.Minute == 00 && tm.Hour == 23){
     digitalWrite(deadManTx, LOW);
+    // Sends signal to board to shutdown and prepare for reboot.
     digitalWrite(forceDown, HIGH);
+    // Serial output for development feedback.
     Serial.write("Initiating daily reset. Waiting for ZedBoard to power off.");
     Serial.println();
+    // Waits 1 minute for board to shutdown. Turns off power, and waits 3 seconds.
     delay(60000);
     digitalWrite(relayControl, LOW);
     digitalWrite(forceDown, LOW);
     delay(3000);
   }
+  // Script checks time every second.
   delay(1000);
   
 }
