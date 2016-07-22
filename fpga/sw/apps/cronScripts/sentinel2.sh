8e0e1ebaea26b43a068b61345a2deed65591e3cf
Chris Turner <cturner48@gatech.edu>
2016-07-22 15:42:19 -0400

    Sentinel screipt 2 merge.

--- fpga/sw/apps/cronScripts/sentinel2.sh
+++ fpga/sw/apps/cronScripts/sentinel2.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
+# Health Check #
+# Statement copies the value of gpio960 to gpio963. gpio960 is the output from
+# the arduino health check statement, and gpio963 is the output from the FPGA
+# to the arduino to cancel the health check reboot.
 SHUTDOWN="$(cat /sys/class/gpio/gpio960/value)"
 if [ $(cat /sys/class/gpio/gpio960/value) = 1 ]
 then
-    echo "Shutdown request stopped by sentinel script."
+    # Sensor log output.
+    echo "Shutdown request stopped by sentinel script." >> /home/ubuntu/ev/sensor.log
 fi
 
 echo $SHUTDOWN > /sys/class/gpio/gpio963/value
 
 
+# Force Shutdown #
+# If the force shutdown signal is recieved as a high value on gpio961
+# the board writes to the sensor log and then powers off waiting
+# for the power reset.
 if [ $(cat /sys/class/gpio/gpio961/value) = 1 ]
 then
-    echo "Powering off for daily reset." 
+    echo "Powering off for daily reset." >> /home/ubuntu/ev/sensor.log
     sudo poweroff
     
 fi
