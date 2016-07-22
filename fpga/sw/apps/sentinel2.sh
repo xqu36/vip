27fc9841d1c4812a1a276b65e3d1606f525022ea
cturner48 <cturner48@gatech.edu>
2016-07-22 11:47:46 -0400

    Script changes for merge.

--- fpga/sw/apps/sentinel2.sh
+++ fpga/sw/apps/sentinel2.sh
@@ -3,7 +3,7 @@
 SHUTDOWN="$(cat /sys/class/gpio/gpio960/value)"
 if [ $(cat /sys/class/gpio/gpio960/value) = 1 ]
 then
-    echo "Shutdown request stopped by sentinel script."
+    echo "Shutdown request stopped by sentinel script. $(date)" >> /home/ubuntu/ev/sensor.log
 fi
 
 echo $SHUTDOWN > /sys/class/gpio/gpio963/value
@@ -11,7 +11,7 @@ echo $SHUTDOWN > /sys/class/gpio/gpio963/value
 
 if [ $(cat /sys/class/gpio/gpio961/value) = 1 ]
 then
-    echo "Powering off for daily reset." 
+    echo "Powering off for daily reset. $(date)" >> /home/ubuntu/ev/sensor.log 
     sudo poweroff
     
 fi
