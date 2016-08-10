081065b492cc8e639f17cd32db51e1e5b33d97fb
Ryan Daniel Beckmann <rbeckmann3@gatech.edu>
2016-08-10 11:55:34 -0400

    update

---
+++ fpga/sw/apps/deploy/compair2.py
@@ -0,0 +1,58 @@
+import os
+os.system('dpkg --get-selections | grep -v deinstall > /home/ubuntu/deploy/allpacks.txt') 
+os.system('sudo rm /home/ubuntu/deploy/deletepacks.sh')
+f=open('/home/ubuntu/deploy/allpacks.txt','r')
+i=0
+atall = 0
+for line in f:
+    k=open('/home/ubuntu/deploy/hispacks.txt','r')
+    end = 0
+    j = 0
+    match = 0
+    while end == 0:
+        if line[j] == " " or line[j] == "	":
+            end = 1
+        else:
+            j = j+1
+    inser = line[:j]    #this is the current word in allpacks
+    for line in k.readlines():
+    	check = line.rstrip()
+        if inser == check:
+            match = 1
+            break
+        else:
+            pass
+    if match == 0:
+    	d=open('/home/ubuntu/deploy/deletepacks.sh','a')
+        d.write("echo 'Y' | sudo apt-get purge ")
+        d.write(inser)
+        d.write("\n")
+        d.close()
+        i = i+1
+        atall = 1
+print("done searching")
+if i != 0:
+    print("%d packages found that were not present on V1.1 build" %i)
+    print("\n\nto complete the deployment process, perform one of the actions below:\n")
+    response = raw_input("type d to delete packages or k to keep packages and update the package list")
+    if response == "d":
+        os.system('sudo chmod a+x /home/ubuntu/deploy/deletepacks.sh')
+        os.system('./home/ubuntu/deploy/deletepacks.sh')
+        print("to make these changes perminent, alter the 'removing packages' part of the deployment script with the current deletepacks.sh")
+    else:
+        os.system('sudo rm /home/ubuntu/deploy/hispacks.txt')
+        os.system('dpkg --get-selections | grep -v deinstall > /home/ubuntu/deploy/packs.txt')
+        f=open('/home/ubuntu/deploy/packs.txt','r')
+        for line in f:
+            end = 0
+            j = 0
+            while end == 0:
+                if line[j] == " " or line[j] == "	":
+                    end = 1
+                else:
+                    j = j+1
+            inser = line[:j]
+            d = open('/home/ubuntu/deploy/hispacks.txt','a')
+            d.write(inser)
+            d.write("\n")
+            d.close()
