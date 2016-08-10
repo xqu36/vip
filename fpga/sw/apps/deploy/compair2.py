baab11c66d52ce950283b5b66e881f79eb76c9cf
Ryan Daniel Beckmann <rbeckmann3@gatech.edu>
2016-08-10 15:22:18 -0400

    update

--- fpga/sw/apps/deploy/compair2.py
+++ fpga/sw/apps/deploy/compair2.py
@@ -1,11 +1,11 @@
 import os
-os.system('dpkg --get-selections | grep -v deinstall > /home/ubuntu/deploy/allpacks.txt') 
-os.system('sudo rm /home/ubuntu/deploy/deletepacks.sh')
-f=open('/home/ubuntu/deploy/allpacks.txt','r')
+os.system('dpkg --get-selections | grep -v deinstall > /mnt/ramdisk/apps/deploy/allpacks.txt') 
+os.system('sudo rm /mnt/ramdisk/apps/deploy/deletepacks.sh')
+f=open('/mnt/ramdisk/apps/deploy/allpacks.txt','r')
 i=0
 atall = 0
 for line in f:
-    k=open('/home/ubuntu/deploy/hispacks.txt','r')
+    k=open('/mnt/ramdisk/apps/deploy/hispacks.txt','r')
     end = 0
     j = 0
     match = 0
@@ -23,7 +23,7 @@ for line in f:
         else:
             pass
     if match == 0:
-    	d=open('/home/ubuntu/deploy/deletepacks.sh','a')
+    	d=open('/mnt/ramdisk/apps/deploy/deletepacks.sh','a')
         d.write("echo 'Y' | sudo apt-get purge ")
         d.write(inser)
         d.write("\n")
@@ -31,18 +31,19 @@ for line in f:
         i = i+1
         atall = 1
 print("done searching")
+os.system('sudo rm /mnt/ramdisk/apps/allpacks.txt')
 if i != 0:
     print("%d packages found that were not present on V1.1 build" %i)
     print("\n\nto complete the deployment process, perform one of the actions below:\n")
     response = raw_input("type d to delete packages or k to keep packages and update the package list")
     if response == "d":
-        os.system('sudo chmod a+x /home/ubuntu/deploy/deletepacks.sh')
-        os.system('./home/ubuntu/deploy/deletepacks.sh')
+        os.system('sudo chmod a+x /mnt/ramdisk/apps/deploy/deletepacks.sh')
+        os.system('./mnt/ramdisk/apps/deploy/deletepacks.sh')
         print("to make these changes perminent, alter the 'removing packages' part of the deployment script with the current deletepacks.sh")
     else:
-        os.system('sudo rm /home/ubuntu/deploy/hispacks.txt')
-        os.system('dpkg --get-selections | grep -v deinstall > /home/ubuntu/deploy/packs.txt')
-        f=open('/home/ubuntu/deploy/packs.txt','r')
+        os.system('sudo rm /mnt/ramdisk/apps/deploy/hispacks.txt')
+        os.system('dpkg --get-selections | grep -v deinstall > /mnt/ramdisk/apps/deploy/packs.txt')
+        f=open('/mnt/ramdisk/apps/deploy/packs.txt','r')
         for line in f:
             end = 0
             j = 0
@@ -52,7 +53,7 @@ if i != 0:
                 else:
                     j = j+1
             inser = line[:j]
-            d = open('/home/ubuntu/deploy/hispacks.txt','a')
+            d = open('/mnt/ramdisk/apps/deploy/hispacks.txt','a')
             d.write(inser)
             d.write("\n")
             d.close()
